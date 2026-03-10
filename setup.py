6542065bf43638c6f6a172207ee56fe4705c28d8
jorisschellekens <joris.schellekens.1989@gmail.com>
2026-03-10 23:26:28 +0100

    modify setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,58 @@ import setuptools  # type: ignore[import-untyped]
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# requirements
-required = [
-    "setuptools>=51.1.1",  # standard
-]
-
 setuptools.setup(
-    name="borb",
-    version="3.0.6",
     author="Joris Schellekens",
     author_email="joris.schellekens.1989@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Markup :: PDF",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
     description="borb is a library for reading, creating and manipulating PDF files in python.",
+    extras_require={
+        "dev": [
+            "black",
+            "mypy",
+            "pydocstyle",
+        ],
+        "docs": [
+            "sphinx",
+            "sphinx-rtd-theme",
+        ],
+        "full": [
+            "cryptography",
+            "ffmpeg-python",
+            "fonttools",
+            "matplotlib",
+            "Pillow",
+            "python-barcode",
+            "py_avataaars",
+            "requests",
+            "segno",
+        ],
+    },
+    include_package_data=True,
+    install_requires=[],
+    keywords=["pdf", "pdf-generation", "pdf-processing", "borb"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jorisschellekens/borb",
+    name="borb",
     packages=setuptools.find_packages(include=["borb", "borb.*"]),
-    include_package_data=True,
-    install_requires=required,
+    project_urls={
+        "Source": "https://github.com/borb-pdf/borb",
+        "Tracker": "https://github.com/borb-pdf/borb/issues",
+    },
     python_requires=">=3.6",
+    url="https://github.com/borb-pdf/borb",
+    version="3.0.6",
 )
